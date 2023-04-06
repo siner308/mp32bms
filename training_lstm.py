acdc4e39fd38637896f834a7b8bc584237bdc574
siner308 <siner308@gmail.com>
2023-04-06 21:04:08 +0900

    apply changed feature length, add tensorboard to visualization

--- training_lstm.py
+++ training_lstm.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -25,22 +26,22 @@ class LSTMModel(nn.Module):
 
 
 class CustomDataset(Dataset):
-    def __init__(self, csv_file):
-        self.data = pandas.read_csv(csv_file)
+    def __init__(self, csv_file, length=None):
+        self.data = pandas.read_csv(csv_file, nrows=length)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        input_duration = self.data.iloc[idx]['input_duration']
-        input_difficulty = self.data.iloc[idx]['input_difficulty']
+        # input_duration = self.data.iloc[idx]['input_duration']
+        # input_difficulty = self.data.iloc[idx]['input_difficulty']
         input_level = self.data.iloc[idx]['input_level']
-        output_duration = self.data.iloc[idx]['output_duration']
-        name = self.data.iloc[idx]['name']
-        input_onset = list(self.data.iloc[idx]['input_onset_0':'input_onset_49'].values)
-        output_columns = list(self.data.iloc[idx]['output_columns_0':'output_columns_399'].values)
+        # output_duration = self.data.iloc[idx]['output_duration']
+        # name = self.data.iloc[idx]['name']
+        input_onset = list(self.data.iloc[idx]['input_onset_0':'input_onset_499'].values)
+        output_columns = list(self.data.iloc[idx]['output_columns_0':'output_columns_3999'].values)
 
-        input = [input_level, input_difficulty] + input_onset
+        input = [input_level] + input_onset
         output = output_columns
 
         return torch.tensor(input, dtype=torch.float32), torch.tensor(output, dtype=torch.float32)
@@ -48,22 +49,23 @@ class CustomDataset(Dataset):
 
 if __name__ == "__main__":
     # 모델 초기화
-    input_size = 52  # 입력 데이터의 크기 (50개의 onset 데이터 + level + difficulty)
+    input_size = 501  # 입력 데이터의 크기 (500개의 onset 데이터 + level)
     hidden_size = 128  # LSTM의 히든 레이어 크기
     num_layers = 2  # LSTM의 레이어 수
-    output_size = 400  # 출력 데이터의 크기 (8개의 lane * 50개의 시간 단계)
+    output_size = 4000  # 출력 데이터의 크기 (8개의 lane * 500개의 시간 단계)
     model = LSTMModel(input_size, hidden_size, num_layers, output_size).to(device)
 
     # 손실 함수와 최적화 함수 정의
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.02)
+    num_epochs = 100
+    optimizer = optim.Adam(model.parameters(), lr=1 / num_epochs)
 
     # data from training_set.csv
-    train_dataset = CustomDataset('training_set_backfront.csv')
+    train_dataset = CustomDataset('training_set_five_seconds.csv')
     train_loader = DataLoader(dataset=train_dataset, batch_size=100, shuffle=True)
 
+    writer = SummaryWriter()
     # 학습 루프
-    num_epochs = 50
     for epoch in range(num_epochs):
         print('epoch: ' + str(epoch))
         for i, (input_seq, target_seq) in enumerate(iter(train_loader)):
@@ -76,6 +78,7 @@ if __name__ == "__main__":
 
             # 손실 계산 및 역전파
             loss = criterion(output_seq, target_seq)
+            writer.add_scalar('Loss/train', loss, epoch)
             loss.backward()
             optimizer.step()
 
@@ -84,6 +87,9 @@ if __name__ == "__main__":
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                       .format(epoch + 1, num_epochs, i + 1, len(train_loader), loss.item()))
 
+    writer.flush()
+    writer.close()
+
     # save model
     torch.save(model.state_dict(), 'lstm_model.pth')
-    
+
