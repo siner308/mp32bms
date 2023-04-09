6a288fd155dd690f7fde90c7b219ba6c440ef8af
siner308 <siner308@gmail.com>
2023-04-09 13:59:26 +0900

    update training_set_files script

--- utils/merge_training_set_files.py
+++ utils/merge_training_set_files.py
@@ -3,22 +3,39 @@ import pandas
 
 def merge_training_set():
     training_sets = [
-        # "./training_set_A.csv",
-        # "./training_set_B.csv",
-        # "./training_set_C.csv",
-        # "./training_set_D.csv",
-        # "./training_set_E.csv",
-        # "./training_set_M.csv",
-        # "./training_set_S.csv",
-        "./back_training_set.csv",
-        "./front_training_set.csv",
+        "./training_set_ten_seconds_A.csv",
+        "./training_set_ten_seconds_B.csv",
+        "./training_set_ten_seconds_C.csv",
+        "./training_set_ten_seconds_D.csv",
+        "./training_set_ten_seconds_E.csv",
+        "./training_set_ten_seconds_F.csv",
+        "./training_set_ten_seconds_G.csv",
+        "./training_set_ten_seconds_H.csv",
+        "./training_set_ten_seconds_I.csv",
+        "./training_set_ten_seconds_J.csv",
+        "./training_set_ten_seconds_K.csv",
+        "./training_set_ten_seconds_L.csv",
+        "./training_set_ten_seconds_M.csv",
+        "./training_set_ten_seconds_N.csv",
+        "./training_set_ten_seconds_O.csv",
+        "./training_set_ten_seconds_P.csv",
+        "./training_set_ten_seconds_Q.csv",
+        "./training_set_ten_seconds_R.csv",
+        "./training_set_ten_seconds_S.csv",
+        "./training_set_ten_seconds_T.csv",
+        "./training_set_ten_seconds_U.csv",
+        "./training_set_ten_seconds_V.csv",
+        "./training_set_ten_seconds_W.csv",
+        "./training_set_ten_seconds_X.csv",
+        "./training_set_ten_seconds_Y.csv",
+        "./training_set_ten_seconds_Z.csv",
     ]
 
     training_set = pandas.DataFrame()
     for training_set_path in training_sets:
         training_set = pandas.concat([training_set, pandas.read_csv(training_set_path)])
 
-    training_set.to_csv("./training_set_backfront.csv", index=False)
+    training_set.to_csv("./training_set_ten_seconds.csv", index=False)
 
 
 if __name__ == "__main__":
